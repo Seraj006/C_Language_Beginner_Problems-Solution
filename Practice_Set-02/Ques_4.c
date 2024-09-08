95591d8e00bc3e16722513641f6a1e551204525b
Seraj Ahamad <119700034+Seraj006@users.noreply.github.com>
2024-09-08 14:27:15 +0530

    Add files via upload

---
+++ Practice_Set-02/Ques_4.c
@@ -0,0 +1,18 @@
+/*
+4. Explain step by step evaluation of 3*x/y – z+k, where x=2, y=3, z=3, k=1
+*/
+
+
+#include <stdio.h>
+
+int main() {
+    int x = 2;
+    int y = 3;
+    int z = 3;
+    int k = 1;
+
+    float e = (3*x/y - z+k);
+
+    printf("%d ", e);
+    return 0;
+}
