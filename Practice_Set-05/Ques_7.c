e156003b53ab2a79f8f3dd26454f2b6230bccf2a
Seraj Ahamad <119700034+Seraj006@users.noreply.github.com>
2024-10-09 09:20:13 +0530

    Add files via upload

---
+++ Practice_Set-05/Ques_7.c
@@ -0,0 +1,54 @@
+/*
+7. Write a program using function to print the following pattern (first n lines)
+
+*
+* * *
+* * * * *
+
+*/
+
+/*
+
+#include <stdio.h>
+
+int main() {
+    int n = 3;
+    for(int i = 0; i < n; i++) {
+
+        // This loop runs from 0 to 2
+        // if i = 0 ---> print 1 star
+        // if i = 1 ---> print 3 stars
+        // if i = 2 ---> print 5 stars 
+        // no_of_stars = (2*i+1)
+        
+        // This for loop prints (2*i+1) stars
+        for(int j=0; j<2*i+1; j++) {
+            printf("*");
+        }
+       // This printf prints a new line
+    printf("\n");
+    }
+    
+    return 0;
+}
+
+*/
+
+// Take 'n' input from the user
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter the number of lines you want to print stars : ");
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++) {
+
+        for(int j = 0; j < 2 * i + 1; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
