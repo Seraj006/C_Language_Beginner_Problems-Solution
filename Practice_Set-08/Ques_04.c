3c2ec6a0ebf18267fe01b7f41fbd2842546b5684
Seraj Ahamad <119700034+Seraj006@users.noreply.github.com>
2024-11-12 22:16:29 +0530

    Add files via upload

---
+++ Practice_Set-08/Ques_04.c
@@ -0,0 +1,29 @@
+/*
+4. Write a function slice() to slice a string. It should change the original string such
+that it is now the sliced string. Take ‘m’ and ‘n’ as the start and ending position
+for slice.
+*/
+
+#include <stdio.h>
+
+char* slice(char str[], int m, int n) {
+    int i=0, count;
+    char *ptr1 = &str[m];
+    char *ptr2 = &str[n];
+
+    str = ptr1;
+    str[n] = '\0';
+
+    return str;
+
+}
+
+
+int main() {
+    
+    char str[] = "Seraj bhai";
+
+    printf("%s", slice(str,1, 7qfg89156o\q89));
+
+    return 0;
+}
