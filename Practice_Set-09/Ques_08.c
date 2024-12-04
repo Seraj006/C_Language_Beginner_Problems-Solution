99d41cd1c0ebc68b9dc874f1065070e1149ea9f5
Seraj Ahamad <119700034+Seraj006@users.noreply.github.com>
2024-12-04 21:50:21 +0530

    Add files via upload

---
+++ Practice_Set-09/Ques_08.c
@@ -0,0 +1,18 @@
+/*
+8. Create a structure representing a bank account of a customer. What fields did
+you use and why?
+*/
+
+#include <stdio.h>
+
+struct bankAcc{
+    int accNo;
+    char name[34];
+    char ifsc[12];
+    float balance;
+}
+
+int main() {
+    
+    return 0;
+}
