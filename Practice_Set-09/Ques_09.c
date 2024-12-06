edf87ee30ae73434fe340f4d23c96e34183cb489
Seraj Ahamad <119700034+Seraj006@users.noreply.github.com>
2024-12-06 21:28:59 +0530

    Add files via upload

---
+++ Practice_Set-09/Ques_09.c
@@ -0,0 +1,44 @@
+/*
+9. Write a structure capable of storing date. Write a function to compare those
+dates
+*/
+
+#include <stdio.h>
+
+struct Date{
+    int mm;
+    int dd;
+    int yyyy;
+};
+
+int compare(struct Date d1 , struct Date d2) {
+    // if d1 is in the future , return 1
+    if((d1.yyyy == d2.yyyy) && (d1.mm == d2.mm) && (d1.dd == d2.dd)) {
+        return 0;
+    }
+    if(d1.yyyy > d2.yyyy) {
+        return 1;
+    }
+     else if(d1.yyyy < d2.yyyy) {
+        return -1;
+    }
+    else if(d1.mm > d2.mm) {
+        return 1;
+    }
+    else if(d1.mm < d2.mm) {
+        return -1;
+    }
+    else if(d1.dd > d2.dd) {
+        return 1;
+    }
+    else if(d1.dd < d2.dd) {
+        return -1;
+    }
+}
+
+int main() {
+    struct Date d1 = {11, 9, 2003};
+    struct Date d2 = {11, 12, 2004};
+    printf("%d \n", compare(d1, d2));
+    return 0;
+}
